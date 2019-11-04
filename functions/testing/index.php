90f2650bbf718c65570db399b370bebb6fcce6b4
Mohannad Hisham <Mohannadize15@gmail.com>
2019-11-04 02:58:20 +0200

    getting paypal files ready

---
+++ functions/testing/index.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set("display_errors",0);
+
+require('PaypalIPN.php');
+
+use PaypalIPN;
+
+$ipn = new PaypalIPN();
+
+// // Use the sandbox endpoint during testing.
+$ipn->useSandbox();
+// $verified = $_POST['payment_status'] === "Completed";
+$verified = $ipn->verifyIPN();
+if ($verified) {
+    $arraii = [
+        "verified" => $verified,
+        "POST" => $_POST,
+        "phpInput" => file_get_contents("php://input"),
+        "SERVER" => $_SERVER,
+        "REQUEST" => $_REQUEST,
+        "COOKIE" => $_COOKIE,
+        "GET" => $_GET,
+        "SESSION" => $_SESSION
+    ];
+    file_put_contents("log.json", json_encode($arraii).PHP_EOL, FILE_APPEND);
+}
+
+// // Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
+header("HTTP/1.1 200 OK");
+
+?>
